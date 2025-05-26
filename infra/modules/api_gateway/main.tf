25600093328866eb62f26232884eecf4b310be4c
stefd <stefanysantossilva.10@gmail.com>
2025-05-26 13:41:39 -0300

    [INFRA] Remoção de item duplicado

--- infra/modules/api_gateway/main.tf
+++ infra/modules/api_gateway/main.tf
@@ -45,7 +45,7 @@ resource "aws_api_gateway_integration" "validate_integration" {
   http_method             = aws_api_gateway_method.post_password_validate.http_method
   integration_http_method = "POST"
   type                    = "HTTP_PROXY"
-  uri                     = var.integration_uri
+  uri                     = "http://${var.integration_uri}/api/v1/password/validate"
   connection_type         = "VPC_LINK"
   connection_id           = aws_api_gateway_vpc_link.ecs_vpc_link.id
   depends_on = [
